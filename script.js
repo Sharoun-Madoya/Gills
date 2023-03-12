12bdd3fe5290bdfc0177297710f629828a0f226a
[sharoun <[madoyasharoun@gmail.com]>
2023-03-12 15:15:46 +0300

    First Page

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+const h1= document.createElement("h2");
+h1.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const menu = document.querySelector('mobile-menu')
+const menulinks =document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function() {
+    menu.classList.toogle('is active');
+    menulinks.classList.toggle('active');
+})
